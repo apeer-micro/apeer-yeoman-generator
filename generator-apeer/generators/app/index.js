b8d4c92367b7a8d62191a0ef8e9606f9fdbe1f2b
thomasirmer <thomasirmer@gmail.com>
2019-01-28 00:17:44 +0100

    Adds single file generation in spec

--- generator-apeer/generators/app/index.js
+++ generator-apeer/generators/app/index.js
@@ -7,6 +7,7 @@ module.exports = class extends Generator {
     }
 
     initializing() {
+        // module properties
         this.general_module_info = {
             module_name: '',
             programming_language: ''
@@ -40,13 +41,10 @@ module.exports = class extends Generator {
             type: 'list',
             name: 'programming_language',
             message: 'Your module\'s programming language',
-            choices: [{
-                name: 'Python',
-                value: 'python'
-            }, {
-                name: 'MATLAB',
-                value: 'matlab'
-            }],
+            choices: [
+                { name: 'Python', value: 'python' },
+                { name: 'MATLAB', value: 'matlab' }
+            ],
             default: 'python'
         }]).then(answers => {
             this.general_module_info.module_name = answers.module_name;
@@ -59,103 +57,79 @@ module.exports = class extends Generator {
             type: 'list',
             name: 'spec_item',
             message: 'Your module\'s specification',
-            choices: [{
-                name: 'Add an input',
-                value: 'input'
-            }, {
-                name: 'Add an output',
-                value: 'output'
-            }, {
-                name: 'Add a requirement',
-                value: 'requirement'
-            }, {
-                name: 'Done',
-                value: 'done'
-            }],
+            choices: [
+                { name: 'Add an input', value: 'inputs' },
+                { name: 'Add an output', value: 'outputs' },
+                { name: 'Add a requirement', value: 'requirements' },
+                { name: 'Done', value: 'done' }
+            ],
             default: 'input',
             store: true
         }, {
             when: function (answers) {
-                return answers.spec_item == 'input'
+                return answers.spec_item == 'inputs'
             },
             type: 'list',
             name: 'spec_item_type',
             message: 'What kind of input',
-            choices: [{
-                name: 'Single file',
-                value: 'type:file'
-            }, {
-                name: 'Multiple files',
-                value: 'type:list[file]'
-            }, {
-                name: 'Text',
-                value: 'type:string'
-            }, {
-                name: 'Integer',
-                value: 'type:integer'
-            }, {
-                name: 'Floating point number',
-                value: 'type:number'
-            }, {
-                name: 'List of floating point numbers',
-                value: 'type:list[number]'
-            }, {
-                name: 'Numerical range',
-                value: 'type:range'
-            }, {
-                name: 'Yes/no choice',
-                value: 'type:choice_binary'
-            }, {
-                name: 'Single choice',
-                value: 'type:choice_single'
-            }],
+            choices: [
+                { name: 'Single file', value: 'type:file' },
+                { name: 'Multiple files', value: 'type:list[file]' },
+                { name: 'Text', value: 'type:string' },
+                { name: 'Integer', value: 'type:integer' },
+                { name: 'Floating point number', value: 'type:number' },
+                { name: 'List of floating point numbers', value: 'type:list[number]' },
+                { name: 'Numerical range', value: 'type:range' },
+                { name: 'Yes/no choice', value: 'type:choice_binary' },
+                { name: 'Single choice', value: 'type:choice_single' },
+            ],
             default: 'file',
             store: true
         }, {
             when: function (answers) {
-                return answers.spec_item == 'output'
+                return answers.spec_item == 'outputs'
             },
             type: 'list',
             name: 'spec_item_type',
             message: 'What kind of output',
-            choices: [{
-                name: 'Single file',
-                value: 'type:file'
-            }, {
-                name: 'Multiple files',
-                value: 'type:list[file]'
-            }, {
-                name: 'Text',
-                value: 'type:string'
-            }, {
-                name: 'Integer',
-                value: 'type:integer'
-            }, {
-                name: 'Floating point number',
-                value: 'type:number'
-            }, {
-                name: 'List of floating point numbers',
-                value: 'type:list[number]'
-            }, {
-                name: 'Numerical range',
-                value: 'type:range'
-            }],
+            choices: [
+                { name: 'Single file', value: 'type:file' },
+                { name: 'Multiple files', value: 'type:list[file]' },
+                { name: 'Text', value: 'type:string' },
+                { name: 'Integer', value: 'type:integer' },
+                { name: 'Floating point number', value: 'type:number' },
+                { name: 'List of floating point numbers', value: 'type:list[number]' },
+                { name: 'Numerical range', value: 'type:range' },
+            ],
             default: 'file',
             store: true
         }, {
             when: function (answers) {
-                return answers.spec_item == 'requirement'
+                return answers.spec_item == 'inputs' || answers.spec_item == 'outputs';
+            },
+            type: 'input',
+            name: 'spec_item_name',
+            message: 'What is the name',
+            // TODO: validate input name
+        }, {
+            when: function (answers) {
+                return answers.spec_item == 'requirements'
             },
             type: 'list',
             name: 'spec_item_type',
             message: 'What kind of requirement',
-            choices: [{
-                name: 'Graphics processor (GPU)',
-                value: 'gpu'
-            }, {
-                name: 'Internet access',
-                value: 'internet_access'
-            }]
+            choices: [
+                { name: 'Graphics processor (GPU)', value: 'gpu' },
+                { name: 'Internet access', value: 'internet_access' }
+            ]
+        }, {
+            when: function (answers) {
+                return answers.spec_item_type == 'type:file';
+            },
+            type: 'checkbox',
+            name: 'file_formats',
+            message: 'Select the supported file types',
+            choices: ['jpg', 'png', 'tif', 'czi'],
         }]);
 
         const done = this.async();
@@ -165,19 +139,34 @@ module.exports = class extends Generator {
                 return;
             }
             switch (answers.spec_item) {
-                case 'input':
+                case 'inputs':
+                case 'outputs':
+                    this._add_parameter(answers);
                     break;
-                case 'output':
-                    break;
-                case 'requirement':
-                    if (answers.spec_item_type == 'gpu')
-                        this.module_spec.requirements.hardware[answers.spec_item_type] = {};
-                    else if (answers.spec_item_type == 'internet_access')
-                        this.module_spec.requirements.network[answers.spec_item_type] = {};
+                case 'requirements':
+                    this._add_requirement(answers);
                     break;
             }
             this.prompting_module_specification();
         });
+    }
+
+    _add_requirement(answers) {
+        if (answers.spec_item_type == 'gpu')
+            this.module_spec.requirements.hardware[answers.spec_item_type] = {};
+        else if (answers.spec_item_type == 'internet_access')
+            this.module_spec.requirements.network[answers.spec_item_type] = {};
+    }
+
+    _add_parameter(answers) {
+        this.module_spec.spec[answers.spec_item][answers.spec_item_name] = {};
+        this.module_spec.spec[answers.spec_item][answers.spec_item_name][answers.spec_item_type] = {};
+
+        if (answers.spec_item_type == 'type:file') {
+            this.module_spec.spec[answers.spec_item][answers.spec_item_name][answers.spec_item_type] = {
+                format: answers.file_formats
+            };
+        };
     }
 
     configuring() { }

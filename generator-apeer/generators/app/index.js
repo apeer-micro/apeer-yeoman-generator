ed981769fbd3cef12f5810a703dd59134eb58895
Thomas Irmer <thomasirmer@gmail.com>
2019-01-27 17:39:52 +0100

    Prepare for module specification prompt flow

--- generator-apeer/generators/app/index.js
+++ generator-apeer/generators/app/index.js
@@ -1,26 +1,87 @@
 ﻿var Generator = require('yeoman-generator');
+const yosay = require('yosay');
 
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
     }
 
-    initializing() { }
+    initializing() {
+        this.general_module_info = {
+            module_name: '',
+            programming_language: ''
+        };
+        this.module_docker = {};
+        this.module_spec = {
+            spec: {
+                inputs: {},
+                outputs: {}
+            },
+            ui: {
+                inputs: {},
+                outputs: {}
+            },
+            requirements: {}
+        };
+    }
 
-    async prompting() {
-        this.answers = await this.prompt([{
+    prompting_general_info() {
+        this.log(yosay('Welcome to the APEER module generator'));
+
+        return this.prompt([{
             type: 'input',
             name: 'module_name',
-            message: 'Your module name',
+            message: 'Your module\'s name',
             default: 'my-apeer-module'
         }, {
             type: 'list',
             name: 'programming_language',
-            message: 'Programming language',
-            choices: ['Python', 'MATLAB'],
-            default: 'Python',
+            message: 'Your module\'s programming language',
+            choices: [{
+                name: 'Python',
+                value: 'python'
+            }, {
+                name: 'MATLAB',
+                value: 'matlab'
+            }],
+            default: 'python'
+        }]).then(answers => {
+            this.general_module_info.module_name = answers.module_name;
+            this.general_module_info.programming_language = answers.programming_language;
+        });
+    }
+
+    prompting_module_specification() {
+        const spec_prompt = ([{
+            type: 'list',
+            name: 'spec_item_type',
+            message: 'Your module\'s specification',
+            choices: [{
+                name: 'Add an input',
+                value: 'input'
+            }, {
+                name: 'Add an output',
+                value: 'output'
+            }, {
+                name: 'Add a requirement',
+                value: 'requirement'
+            }, {
+                name: 'Done',
+                value: 'done'
+            }],
+            default: 'input',
             store: true
-        },]);
+        }]);
+
+        const done = this.async();
+        return this.prompt(spec_prompt).then(answers => {
+            if (answers.spec_item_type == 'done') {
+                done();
+                return;
+            }
+            // TODO prompt for details and add input/output/req to spec
+            this.prompting_module_specification();
+        });
     }
 
     configuring() { }
@@ -28,11 +89,11 @@ module.exports = class extends Generator {
     default() { }
 
     writing() {
-        switch (this.answers.programming_language) {
-            case 'Python':
+        switch (this.general_module_info.programming_language) {
+            case 'python':
                 this.sourceRoot(this.templatePath() + '/python')
                 break;
-            case 'MATLAB':
+            case 'matlab':
                 this.sourceRoot(this.templatePath() + '/matlab')
                 break;
         }
